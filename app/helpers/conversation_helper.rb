d19eecc9deaf08afca9fb55c87d7cbc165cfa009
Brooks Swinnerton <bswinnerton@gmail.com>
2014-06-30 20:29:23 -0400

    Moving date logic to view helper

--- app/helpers/conversation_helper.rb
+++ app/helpers/conversation_helper.rb
@@ -1,2 +1,5 @@
 module ConversationHelper
+  def conversation_friendly_date(post)
+    post.received_at.strftime("%B #{post.received_at.day.ordinalize} %Y at %l:%M%p")
+  end
 end
--- app/models/sender.rb
+++ app/models/sender.rb
@@ -2,6 +2,7 @@ class Sender < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :email
+
   def self.find_or_create_for_mandrill_event!(email, name)
     find_or_create_by!(email: email) do |s|
       s.name = name
